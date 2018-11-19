4b7a9db6052280bc8892a70379cf3225a3f40749
denniscarr <dcarr1@gmail.com>
2018-11-18 23:30:54 -0500

    Added collision for deer.

--- Dear Headlights,/Assets/Scripts/Driving/RoadObject.cs
+++ Dear Headlights,/Assets/Scripts/Driving/RoadObject.cs
@@ -9,15 +9,21 @@ public class RoadObject : MonoBehaviour {
     [SerializeField] AnimationCurve approachCurve;
     [SerializeField] float approachSpeed = 0.1f;
     [SerializeField] bool loop;
+    [SerializeField] bool isCollidable;
 
     [HideInInspector] public float currentDistance;
-    Vector3 originalScale;
+
+    private Vector3 originalScale;
+    private bool deleteTag = false;
 
     private void Awake() {
         originalScale = transform.localScale;
     }
 
+    Vector3 previousPosition = Vector3.zero;
     private void Update() {
+        currentDistance = Mathf.Clamp01(currentDistance + approachSpeed * Time.deltaTime * Services.car.currentSpeed);
+
         float yPosition = approachCurve.Evaluate(currentDistance);
         yPosition = Den.Math.Map(yPosition, 0f, 1f, Services.roadRenderer.horizon, Services.roadRenderer.leftEdgeCurve.lowerPoint.y);
 
@@ -26,18 +32,24 @@ public class RoadObject : MonoBehaviour {
         float newScale = Den.Math.Map(yPosition, Services.roadRenderer.horizon, Services.roadRenderer.leftEdgeCurve.lowerPoint.y, 0.01f, maxScale);
         transform.localScale = originalScale * newScale;
 
-        currentDistance += approachSpeed * Time.deltaTime * Services.car.currentSpeed;
-
         GetComponent<SpriteRenderer>().sortingOrder = Mathf.FloorToInt(Den.Math.Map(currentDistance, 0f, 1f, 0f, 10000f));
 
-        if (currentDistance > 1) {
+        if (currentDistance >= 0.99) {
+            if (isCollidable) {
+                // See if we are colliding with the player.
+                float viewportX = Camera.main.WorldToViewportPoint(previousPosition).x;
+                if (viewportX >= 0.3f && viewportX < 0.6f) {
+                    Services.gameManager.Crash();
+                }
+            }
+
             RemoveSelf();
         }
+
+        previousPosition = transform.position;
     }
 
     void RemoveSelf() {
-        // See if we are colliding with the player.
-        
 
         if (loop) {
             currentDistance = 0f;
