303d5cc09566761dc2eddb38bfbe1da2876e79ac
uctuviun <gusboehling@gmail.com>
2018-11-26 23:18:23 -0500

    engine sound added, linked to speed

---
+++ Dear Headlights,/Assets/Scripts/Driving/engine_sound_speed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class engine_sound_speed : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GetComponent<AudioSource>().pitch = (Den.Math.Map(Services.car.currentSpeed, 0f, Services.car.maxSpeed, 0.2f, 3f));
+
+	}
+}
